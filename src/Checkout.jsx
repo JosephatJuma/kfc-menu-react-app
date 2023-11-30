5bbcfb694de73fba5217fab5a2ba8242930a0a74
Juma Josephat <jumajosephat61@gmail.com>
2023-11-30 22:09:50 +0300

    Remove from card eben on checkout

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
-import { useSelector } from "react-redux";
-import { Paper, Box, Card, Typography, IconButton } from "@mui/material";
-import { ArrowBack } from "@mui/icons-material";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Paper,
+  Box,
+  Card,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
+import { ArrowBack, Remove } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { removeFromCart } from "./redux/slices/cartSlice";
 function Checkout() {
   const themeMode = useSelector((state) => state.theme.mode);
+  const dispatch = useDispatch();
   const card = useSelector((state) => state.cart);
   return (
     <div style={{ backgroundColor: themeMode === "light" && "#f2ebeb" }}>
@@ -26,6 +35,7 @@ function Checkout() {
             </IconButton>
           </Link>
           <h1>Checkout</h1>
+          {card.cart.length === 0 && <Typography>No item in cart</Typography>}
           {card.cart.map((item) => {
             return (
               <Card
@@ -35,16 +45,34 @@ function Checkout() {
                   padding: 2,
                   maxWidth: 1000,
                   flexGrow: 1,
+                  display: "flex",
+                  justifyContent: "space-between",
                 }}
               >
-                <im src={item.image} />
-                <div>
-                  <Typography>{item.name}</Typography>
-                  <Typography>{item.price}</Typography>
+                <div style={{ display: "flex" }}>
+                  <img src={item.image} width={50} height={50} />
+                  <div style={{ marginLeft: 10 }}>
+                    <Typography>{item.name}</Typography>
+                    <Typography>{item.price}</Typography>
+                  </div>
                 </div>
+                <Button
+                  sx={{
+                    backgroundColor: "#000",
+                    color: "#fff",
+                    ":hover": { backgroundColor: "#000000c0" },
+                  }}
+                  endIcon={<Remove />}
+                  onClick={() => {
+                    dispatch(removeFromCart(item));
+                  }}
+                >
+                  Remove
+                </Button>
               </Card>
             );
           })}
+          <Typography>Total: {card.totalAmout}</Typography>
         </Paper>
       </Box>
     </div>
